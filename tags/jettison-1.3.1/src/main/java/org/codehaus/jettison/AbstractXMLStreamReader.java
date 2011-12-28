26e443da333d59588ff275e41d49d2fde6d4b1bb
sergey_beryozkin <sergey_beryozkin@a092bed1-b821-0410-8e71-da2133d526ab>
2011-12-28 10:52:07 +0000

    [maven-release-plugin]  copy for tag jettison-1.3.1

---
+++ tags/jettison-1.3.1/src/main/java/org/codehaus/jettison/AbstractXMLStreamReader.java
@@ -0,0 +1,228 @@
+/**
+ * Copyright 2006 Envoi Solutions LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.jettison;
+
+import java.util.Iterator;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.Location;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+public abstract class AbstractXMLStreamReader implements XMLStreamReader {
+    protected int event;
+    protected Node node;
+    
+    public boolean isAttributeSpecified(int arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean isCharacters() {
+        return event == CHARACTERS;
+    }
+
+    public boolean isEndElement() {
+        return event == END_ELEMENT;
+    }
+
+    public boolean isStandalone() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean isStartElement() {
+        return event == START_ELEMENT;
+    }
+
+    public boolean isWhiteSpace() {
+        return false;
+    }
+
+    public int nextTag() throws XMLStreamException {
+        int event = next();
+        while (event != START_ELEMENT && event != END_ELEMENT) {
+            event = next();
+        }
+        return event;
+    }
+
+    public int getEventType() {
+        return event;
+    }
+
+    public void require(int arg0, String arg1, String arg2) throws XMLStreamException {
+    }
+
+    public int getAttributeCount() {
+        return node.getAttributes().size();
+    }
+
+    public String getAttributeLocalName(int n) {
+        return getAttributeName(n).getLocalPart();
+    }
+
+    public QName getAttributeName(int n) {
+        Iterator itr = node.getAttributes().keySet().iterator();
+        QName name = null;
+        for (int i = 0; i <= n; i++) {
+            name = (QName) itr.next();
+        }
+        return name;
+    }
+
+    public String getAttributeNamespace(int n) {
+        return getAttributeName(n).getNamespaceURI();
+    }
+
+    public String getAttributePrefix(int n) {
+        return getAttributeName(n).getPrefix();
+    }
+
+    public String getAttributeValue(int n) {
+        Iterator itr = node.getAttributes().values().iterator();
+        String name = null;
+        for (int i = 0; i <= n; i++) {
+            name = (String) itr.next();
+        }
+        return name;
+    }
+
+    public String getAttributeValue(String ns, String local) {
+        return (String) node.getAttributes().get(new QName(ns, local));
+    }
+
+    public String getAttributeType(int arg0) {
+        return null;
+    }
+
+    public String getLocalName() {
+        return getName().getLocalPart();
+    }
+
+    public QName getName() {
+        return node.getName();
+    }
+
+    public String getNamespaceURI() {
+        return getName().getNamespaceURI();
+    }
+
+    public int getNamespaceCount() {
+        return node.getNamespaceCount();
+    }
+
+    public String getNamespacePrefix(int n) {
+        return node.getNamespacePrefix(n);
+    }
+
+    public String getNamespaceURI(int n) {
+        return node.getNamespaceURI(n);
+    }
+
+    public String getNamespaceURI(String prefix) {
+        return node.getNamespaceURI(prefix);
+    }
+
+    public boolean hasName() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean hasNext() throws XMLStreamException {
+        return event != END_DOCUMENT;
+    }
+
+    public boolean hasText() {
+        return event == CHARACTERS;
+    }
+
+    public boolean standaloneSet() {
+        return false;
+    }
+
+    public String getCharacterEncodingScheme() {
+        return null;
+    }
+
+    public String getEncoding() {
+        return null;
+    }
+
+    public Location getLocation() {
+        return new Location() {
+
+            public int getCharacterOffset() {
+                return 0;
+            }
+
+            public int getColumnNumber() {
+                return 0;
+            }
+
+            public int getLineNumber() {
+                return -1;
+            }
+
+            public String getPublicId() {
+                return null;
+            }
+
+            public String getSystemId() {
+                return null;
+            }
+            
+        };
+    }
+
+    public String getPIData() {
+        return null;
+    }
+
+    public String getPITarget() {
+        return null;
+    }
+
+    public String getPrefix() {
+        return getName().getPrefix();
+    }
+
+    public Object getProperty(String arg0) throws IllegalArgumentException {
+        return null;
+    }
+
+    public String getVersion() {
+        return null;
+    }
+
+    public char[] getTextCharacters() {
+        return getText().toCharArray();
+    }
+
+    public int getTextCharacters(int sourceStart, char[] target, int targetStart, int length) throws XMLStreamException {
+        getText().getChars(sourceStart,sourceStart+length,target,targetStart);
+        return length;
+    }
+
+    public int getTextLength() {
+        return getText().length();
+    }
+
+    public int getTextStart() {
+        return 0;
+    }
+
+}
---
+++ tags/jettison-1.3.1/src/test/java/org/codehaus/jettison/badgerfish/BadgerFishXMLStreamWriterTest.java
@@ -0,0 +1,265 @@
+/**
+ * Copyright 2006 Envoi Solutions LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.jettison.badgerfish;
+
+import java.io.StringWriter;
+
+import junit.framework.TestCase;
+
+import org.codehaus.jettison.AbstractXMLStreamWriter;
+import org.codehaus.jettison.badgerfish.BadgerFishXMLStreamWriter;
+
+public class BadgerFishXMLStreamWriterTest extends TestCase {
+    public void testRootWithText() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        AbstractXMLStreamWriter w = new BadgerFishXMLStreamWriter(strWriter);
+        
+        w.writeStartDocument();
+        w.writeStartElement("alice");
+        w.writeCharacters("bob");
+        w.writeEndElement();
+        w.writeEndDocument();
+        
+        w.close();
+        strWriter.close();
+        
+        assertJSONEquals("{\"alice\":{\"$\":\"bob\"}}", strWriter.toString());
+    }
+   
+    private void assertJSONEquals(String string, String string2) {
+        string = string.replace(" ", "");
+        string2 = string2.replace(" ", "");
+        
+        assertEquals(string, string2);
+    }
+
+    public void testTwoChildren() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        AbstractXMLStreamWriter w = new BadgerFishXMLStreamWriter(strWriter);
+        
+        w.writeStartDocument();
+        w.writeStartElement("alice");
+
+        w.writeStartElement("bob");
+        w.writeCharacters("charlie");
+        w.writeEndElement();
+        
+        w.writeStartElement("david");
+        w.writeCharacters("edgar");
+        w.writeEndElement();
+        
+        w.writeEndElement();
+        w.writeEndDocument();
+        
+        w.close();
+        strWriter.close();
+        
+        assertJSONEquals("{\"alice\":{\"bob\":{\"$\":\"charlie\"},"+
+                         "\"david\":{\"$\":\"edgar\"}}}", strWriter.toString());
+    }
+    
+    public void testTwoChildrenWithSameName() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        AbstractXMLStreamWriter w = new BadgerFishXMLStreamWriter(strWriter);
+        
+        w.writeStartDocument();
+        w.writeStartElement("alice");
+
+        w.writeStartElement("bob");
+        w.writeCharacters("charlie");
+        w.writeEndElement();
+        
+        w.writeStartElement("bob");
+        w.writeCharacters("david");
+        w.writeEndElement();
+        
+        w.writeEndElement();
+        w.writeEndDocument();
+        
+        w.close();
+        strWriter.close();
+        
+        assertJSONEquals("{\"alice\":{\"bob\":[{\"$\":\"charlie\"},{\"$\":\"david\"}]}}", 
+                         strWriter.toString());
+    }
+
+    public void testMultipleChildrenAgain() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        AbstractXMLStreamWriter xsw = new BadgerFishXMLStreamWriter(strWriter);
+        
+        xsw.writeStartDocument();
+        xsw.writeStartElement("Ratings");
+
+        xsw.writeStartElement("Rating");
+        xsw.writeAttribute("id", "100");
+        xsw.writeAttribute("value", "3");
+        xsw.writeEndElement();
+
+        xsw.writeStartElement("Rating");
+        xsw.writeAttribute("id", "200");
+        xsw.writeAttribute("value", "4");
+        xsw.writeEndElement();
+
+        xsw.writeStartElement("Rating");
+        xsw.writeAttribute("id", "300");
+        xsw.writeAttribute("value", "5");
+        xsw.writeEndElement();
+
+        xsw.writeEndElement();
+        xsw.writeEndDocument ();
+        xsw.flush();
+
+        xsw.close();
+        strWriter.close();
+        System.out.println(strWriter.toString());
+        
+        assertJSONEquals("{\"Ratings\":{\"Rating\":[" +
+                        "{\"@id\":\"100\",\"@value\":\"3\"}," +
+                        "{\"@id\":\"200\",\"@value\":\"4\"}," +
+                        "{\"@id\":\"300\",\"@value\":\"5\"}" +
+                        "]}}", 
+                         strWriter.toString());
+    }
+    
+    public void testAttributeAndText() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        AbstractXMLStreamWriter w = new BadgerFishXMLStreamWriter(strWriter);
+        
+        w.writeStartDocument();
+        w.writeStartElement("alice");
+        
+        w.writeAttribute("charlie", "david");
+        w.writeCharacters("bob");
+        
+        w.writeEndElement();
+        w.writeEndDocument();
+        
+        w.close();
+        strWriter.close();
+        
+        assertJSONEquals("{\"alice\":{\"@charlie\":\"david\",\"$\":\"bob\"}}", 
+                         strWriter.toString());
+    }
+
+    public void testDefaultNamespace() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        BadgerFishXMLStreamWriter w = new BadgerFishXMLStreamWriter(strWriter);
+        
+        w.writeStartDocument();
+        w.writeStartElement("alice");
+        
+        w.writeDefaultNamespace("http://some-namespace");
+        w.writeCharacters("bob");
+        
+        w.writeEndElement();
+        w.writeEndDocument();
+        
+        w.close();
+        strWriter.close();
+        
+        assertJSONEquals("{\"alice\":{\"@xmlns\":{\"$\":\"http:\\/\\/some-namespace\"},\"$\":\"bob\"}}", 
+                         strWriter.toString());
+    }
+    
+    public void testPrefixedNamespace() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        BadgerFishXMLStreamWriter w = new BadgerFishXMLStreamWriter(strWriter);
+        
+        w.writeStartDocument();
+        w.writeStartElement("alice");
+        
+        w.writeDefaultNamespace("http://some-namespace");
+        w.writeNamespace("charlie", "http://some-other-namespace");
+        
+        assertEquals("http://some-other-namespace", w.getNamespaceContext().getNamespaceURI("charlie"));
+        assertEquals("charlie", w.getNamespaceContext().getPrefix("http://some-other-namespace"));
+        
+        w.writeCharacters("bob");
+        
+        w.writeEndElement();
+        w.writeEndDocument();
+        
+        w.close();
+        strWriter.close();
+        
+        assertJSONEquals("{ \"alice\": { \"@xmlns\": { \"$\" : \"http:\\/\\/some-namespace\", \"charlie\" : \"http:\\/\\/some-other-namespace\" }, \"$\" : \"bob\" } }", 
+                         strWriter.toString());
+        
+    }
+    
+
+    
+    public void testPrefixedElements() throws Exception {
+        StringWriter strWriter = new StringWriter();
+        BadgerFishXMLStreamWriter w = new BadgerFishXMLStreamWriter(strWriter);
+        
+        w.writeStartDocument();
+        w.writeStartElement("alice");
+        
+        w.writeDefaultNamespace("http://some-namespace");
+        w.writeNamespace("charlie", "http://some-other-namespace");
+        
+        w.writeStartElement("bob");
+        w.writeCharacters("david");
+        w.writeEndElement();
+        
+
+        w.writeStartElement("charlie", "edgar", "http://some-other-namespace");
+        w.writeCharacters("frank");
+        w.writeEndElement();
+        
+        w.writeEndElement();
+        w.writeEndDocument();
+        
+        w.close();
+        strWriter.close();
+        assertJSONEquals(
+            "{ \"alice\" : { " +
+              "\"@xmlns\" : { " +
+                "\"$\" : \"http:\\/\\/some-namespace\"," +
+                "\"charlie\" : \"http:\\/\\/some-other-namespace\"" +
+              "}, " +
+              "\"bob\" : { " +
+                  "\"$\" : \"david\" " +
+              "}, " +
+              "\"charlie:edgar\" : { " +
+                "\"$\" : \"frank\" " +
+              "} " +
+            "} }", strWriter.toString());
+    }
+    
+    /**
+     * 
+     * <alice><bob>charlie</bob><david>edgar</david></alice>
+     * 
+     * <alice><bob>charlie</bob><bob>david</bob></alice>
+     * { \"alice\": { \"bob\" : [{\"$\": charlie\" }, {\"$\": \"david\" }] } }
+     * 
+     * <alice charlie=\"david\">bob</alice>
+     * { \"alice\": { \"$\" : \"bob\", \"@charlie\" : \"david\" } }
+     * 
+     * <alice xmlns=\"http://some-namespace\">bob</alice>
+     * { \"alice\": { \"$\" : \"bob\", \"@xmlns\": { \"$\" : \"http:\/\/some-namespace\"} } }
+     * 
+     * <alice xmlns=\"http:\/\/some-namespace\" xmlns:charlie=\"http:\/\/some-other-namespace\">bob</alice>
+     * { \"alice\": { \"$\" : \"bob\", \"@xmlns\": { \"$\" : \"http:\/\/some-namespace\", \"charlie\" : \"http:\/\/some-other-namespace\" } } }
+     * 
+     * <alice xmlns=\"http://some-namespace\" xmlns:charlie=\"http://some-other-namespace\"> <bob>david</bob> <charlie:edgar>frank</charlie:edgar> </alice>
+     * { \"alice\" : { \"bob\" : { \"$\" : \"david\" , \"@xmlns\" : {\"charlie\" : \"http:\/\/some-other-namespace\" , \"$\" : \"http:\/\/some-namespace\"} } , \"charlie:edgar\" : { \"$\" : \"frank\" , \"@xmlns\" : {\"charlie\":\"http:\/\/some-other-namespace\", \"$\" : \"http:\/\/some-namespace\"} }, \"@xmlns\" : { \"charlie\" : \"http:\/\/some-other-namespace\", \"$\" : \"http:\/\/some-namespace\"} } }
+     * 
+     * @throws Exception
+     */
+}
