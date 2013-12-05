8cf6aa5ed1a52e531874fd81c55d79c7149cf5ad
sergey_beryozkin <sergey_beryozkin@a092bed1-b821-0410-8e71-da2133d526ab>
2013-12-05 22:21:23 +0000

    [JETTISON-130, JETTISON-131] Fixing illegal array parsing issues

--- trunk/src/test/java/org/codehaus/jettison/json/JSONObjectTest.java
+++ trunk/src/test/java/org/codehaus/jettison/json/JSONObjectTest.java
@@ -1,7 +1,5 @@
 package org.codehaus.jettison.json;
 
-import org.codehaus.jettison.json.JSONObject;
-
 import junit.framework.TestCase;
 
 public class JSONObjectTest extends TestCase {
@@ -23,5 +21,33 @@ public class JSONObjectTest extends TestCase {
     	obj.append("arr", "val2");
     	obj.append("arr", "val3");
     	assertEquals("{\"arr\":[\"val1\",\"val2\",\"val3\"]}", obj.toString());
+    }
+    
+    public void testInvalidArraySequence() throws Exception {
+    	try {
+    	    new JSONObject("{\"a\":[");
+    	    fail("Exception expected");
+    	} catch (JSONException ex) {
+    		assertTrue(ex.getMessage().startsWith("JSONArray text must end with ']'"));
+    	}
+    }
+    
+    public void testInvalidArraySequence2() throws Exception {
+    	try {
+    	    new JSONObject("{\"a\":[,");
+    	    fail("Exception expected");
+    	} catch (JSONException ex) {
+    		assertTrue(ex.getMessage().startsWith("JSONArray text has a trailing ','"));
+    	}
+    }
+    
+    public void testInvalidArraySequence3() throws Exception {
+    	String corruptedJSON = "{\"a\":[[\"b\",{\"revision\": 760839}],";
+    	try {
+    	    new JSONObject(corruptedJSON);
+    	    fail("Exception expected");
+    	} catch (JSONException ex) {
+    		assertTrue(ex.getMessage().startsWith("JSONArray text has a trailing ','"));
+    	}
     }
 }
